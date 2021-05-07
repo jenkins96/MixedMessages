ac1332f3988260c4c5a085898a075bae8abc0aa7
jenkins96 <adrianjnkns@gmail.com>
2021-05-07 23:30:04 +0000

    simple but done

--- MixedMessages/myapp.js
+++ MixedMessages/myapp.js
@@ -2,9 +2,9 @@
 let myProps = {
 	button: document.querySelector("#mybutton"),
 	display: document.querySelector("#displayMessage"),
-	subject: [],
-	verb: [],
-	noun: []
+	subject: ["Mario", "Raul", "Bilma", "Robert", "Halsey"],
+	verb: [" took", " fucked", " ate", " mixed", " ran"],
+	noun: [" the milk", " my car", " the soup", " my notebook", " the t-shirt"]
 
 }
 
@@ -12,12 +12,37 @@ let myProps = {
 let myMethods = {
 
 	init(){
-		myProps.button.addEventListener("click", myMethods.generateMessage)
 
+        myProps.button.addEventListener("click", myMethods.generateMessage)
+        
 	},
 	generateMessage(){
-		//console.log("Joder macho me has clickeado!");
-		myProps.display.innerHTML = "Joder macho me has clickeado!"
-	}
+
+		myProps.display.innerHTML = myMethods.generateSubject() + myMethods.generateVerb() + myMethods.generateNoun();
+    
+    },
+    generateIndex( arr ){
+        
+        return Math.floor(Math.random() * arr.length);
+
+    },
+    generateSubject(){
+
+        let randomNumber = myMethods.generateIndex(myProps.subject);
+        return myProps.subject[ randomNumber ];
+
+    },
+     generateVerb(){
+
+        let randomNumber = myMethods.generateIndex(myProps.verb);
+        return myProps.verb[ randomNumber ];
+
+    },
+     generateNoun(){
+
+        let randomNumber = myMethods.generateIndex(myProps.noun);
+        return myProps.noun[ randomNumber ];
+
+    }
 }
 myMethods.init();
